384faf03546f32d29a7f02cb896289e625c335bb
Aleksander Alekseev <shure348@iMac-Aleksander-2.local>
2017-08-10 23:40:21 +0300

    first

---
+++ dev/js/ready.js
@@ -0,0 +1,44 @@
+'use strict';
+
+$(function(){
+	$('.partners__list').slick({
+		dots: true,
+		slidesToShow: 6,
+		responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        arrows: true,
+		        slidesToShow: 4,
+		        slidesToScroll: 1
+		      }
+		    },{
+		      breakpoint: 480,
+		      settings: {
+		        arrows: true,
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },{
+		      breakpoint: 380,
+		      settings: {
+		        arrows: true,
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    }
+		]
+	});
+
+
+	baguetteBox.run('.baguetteBox');
+});
+
+
+
+
+
+
+
+
+
