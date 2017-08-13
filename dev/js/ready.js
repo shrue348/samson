575657f44bdb5f060f06b77c89fbec8f2b0b9ab7
Aleksander Alekseev <shure348@iMac-Aleksander-2.local>
2017-08-13 21:33:28 +0300

    commit

--- dev/js/ready.js
+++ dev/js/ready.js
@@ -1,40 +1,73 @@
 'use strict';
 
 $(function(){
-	$('.partners__list').slick({
-		dots: true,
-		slidesToShow: 6,
-		responsive: [
-		    {
-		      breakpoint: 768,
-		      settings: {
-		        arrows: true,
-		        slidesToShow: 4,
-		        slidesToScroll: 1
-		      }
-		    },{
-		      breakpoint: 480,
-		      settings: {
-		        arrows: true,
-		        slidesToShow: 3,
-		        slidesToScroll: 1
-		      }
-		    },{
-		      breakpoint: 380,
-		      settings: {
-		        arrows: true,
-		        slidesToShow: 2,
-		        slidesToScroll: 1
-		      }
-		    }
-		]
-	});
-
-
 	baguetteBox.run('.baguetteBox');
+
+	$('.slick_where').slick({
+		slidesToShow: 6,
+		prevArrow: '<button type="button" class="slick_where__prev"><i class="icon-angle-left"></i></button>',
+		nextArrow: '<button type="button" class="slick_where__next"><i class="icon-angle-right"></i></button>',
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+				    slidesToShow: 5
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+				    slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+				    slidesToShow: 2
+			 	}
+			}
+		]
+	})
+
+	$('.slick_main_catitems').slick({
+		slidesToShow: 3,
+		prevArrow: '<button type="button" class="slick_where__prev"><i class="icon-angle-left"></i></button>',
+		nextArrow: '<button type="button" class="slick_where__next"><i class="icon-angle-right"></i></button>',
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+				    slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+				    slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+				    slidesToShow: 1
+			 	}
+			}
+		]
+	})
 });
 
+// $(function(){
+// 	var obj = $('.apartaments_back'),
+// 		img1_offset = obj.offset().top,
+// 		scrolltop;
 
+// 	$(window).scroll(function(e){
+// 		scrolltop = $(window).scrollTop();
+// 		obj.css({'transform': 'translate3d(0, '+(-img1_offset + scrolltop)/6.5+'px, 0)'});
+// 	});
+// });
 
 
 
